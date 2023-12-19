08235b8ffad275b3a2a45cacebd33070baf02459
Bianca Iusztin <bianca_iusztin91@yahoo.com>
2023-12-19 16:46:22 +0200

    Changed db url

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://root:Bianca200491@127.0.0.1:3306/TodoApplicationDatabase'
+SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:Bianca200491@localhost/ToDoApplicationDatabase'
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
