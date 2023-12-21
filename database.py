e15ad6345fcd2b5e4e7e04a3aac893820a160009
Bianca Iusztin <bianca_iusztin91@yahoo.com>
2023-12-21 08:16:04 +0200

    Production database change

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:Bianca200491@localhost/ToDoApplicationDatabase'
+SQLALCHEMY_DATABASE_URL = 'postgresql://kkcshpjf:H-UoO-9MGPazw2KC3sD3FgzO_v6ld1Uu@flora.db.elephantsql.com/kkcshpjf'
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
